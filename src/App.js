3f9ff33b62e088d08471fa3815b00bd7bdb0a0c0
Nick Diamond <nbdiamond@gmail.com>
2022-07-12 21:43:43 -0700

    nick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
+import name from './nick.png';
+
 import './App.css';
 
 function App() {
   return (
+    <Dog></Dog>
+  
+  );
+}
+
+function Dog() {
+  return (
     <div className="App">
       <header className="App-header">
+        <img height={133} src={name} />
         <img src={logo} className="App-logo" alt="logo" />
-     
       </header>
     </div>
   );
