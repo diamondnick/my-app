ba718450ce938bbeb6645899c890787e5ce38a05
Nick Diamond <nbdiamond@gmail.com>
2022-08-01 13:51:38 -0700

    doit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
-import name from './nick.png';
-
 import './App.css';
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import KeyResolver from 'key-did-resolver'
+import { DID } from 'dids'
+import { EthereumAuthProvider, SelfID } from '@self.id/web'
+
+// The following assumes there is an injected `window.ethereum` provider
+const addresses = await window.ethereum.request({
+  method: 'eth_requestAccounts',
+})
+
+// The following configuration assumes your local node is connected to the Clay testnet
+const self = await SelfID.authenticate({
+  authProvider: new EthereumAuthProvider(window.ethereum, addresses[0]),
+  ceramic: 'local',
+  connectNetwork: 'testnet-clay',
+})
+
+await self.set('basicProfile', { name: 'Alice' })
+
+
 
 function App() {
   return (
